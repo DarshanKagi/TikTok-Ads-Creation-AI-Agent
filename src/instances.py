4667b860e1a2e924737361465bb307f954dd7a60
Darshan S Kagi <darshankagi04@gmail.com>
2026-02-03 13:43:14 +0530

    Delete src directory

--- src/instances.py
+++
@@ -1,7 +0,0 @@
-from src.config import USE_REAL_API
-from src.api_mock import MockTikTokAPI
-from src.api_real import RealTikTokAPI
-from src.agent import AdAgent
-
-api_client = RealTikTokAPI() if USE_REAL_API else MockTikTokAPI()
-agent = AdAgent(api_client)
